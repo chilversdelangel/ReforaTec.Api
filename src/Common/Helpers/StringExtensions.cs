8af312c6977683c38447ce6927191c6d9f5be05a
Chilvers Del Angel <l21070316@cdmadero.tecnm.mx>
2026-04-27 13:30:08 -0600

    feat(common): add string sanitization utility

--- src/Common/Helpers/StringExtensions.cs
+++ src/Common/Helpers/StringExtensions.cs
@@ -2,12 +2,14 @@ namespace ReforaTec.Api.Common.Helpers;
 
 public static class StringExtensions
 {
+    public static string ToSanitized(this string value)
+    {
+        var words = value.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return string.Join(" ", words);
+    }
+
     public static string ToNormalized(this string value)
     {
-        var trimmed = value.Trim();
-        var words = trimmed.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        var joined = string.Join(" ", words);
-
-        return joined.ToLowerInvariant();
+        return value.ToSanitized().ToLowerInvariant();
     }
 }
