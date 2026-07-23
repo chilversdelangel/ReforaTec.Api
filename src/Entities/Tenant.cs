fe7888bb7238285275a6a7b1ac3d99431bfc62f3
Chilvers Del Angel <l21070316@cdmadero.tecnm.mx>
2026-07-23 14:54:17 -0600

    feat(entities): add Tenant domain entity

---
+++ src/Entities/Tenant.cs
@@ -0,0 +1,26 @@
+using ReforaTec.Api.Common.Helpers;
+using ReforaTec.Api.Common.Interfaces;
+using ReforaTec.Api.Entities.Common;
+
+namespace ReforaTec.Api.Entities;
+
+public class Tenant : AuditableEntity, INormalizable
+{
+    public string InstitutionName { get; set; } = string.Empty;
+    public string NormalizedInstitutionName { get; set; } = string.Empty;
+    
+    public string InstitutionalEmailDomain { get; set; } = string.Empty;
+    public string NormalizedInstitutionalEmailDomain { get; set; } = string.Empty;
+    
+    public bool IsDeleted { get; set; }
+    public DateTime? DeletedAt { get; set; }
+
+    public void Normalize()
+    {
+        InstitutionName = InstitutionName.ToSanitized();
+        NormalizedInstitutionName = InstitutionName.ToNormalized();
+        
+        InstitutionalEmailDomain = InstitutionalEmailDomain.ToSanitized();
+        NormalizedInstitutionalEmailDomain = InstitutionalEmailDomain.ToNormalized();
+    }
+}
