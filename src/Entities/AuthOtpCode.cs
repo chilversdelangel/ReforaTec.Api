c3693ab04027cca36c23e2b2676aa1666879d6e4
Chilvers Del Angel <l21070316@cdmadero.tecnm.mx>
2026-07-26 11:59:47 -0600

    feat(entities): add FailedAttempts to AuthOtpCode

--- src/Entities/AuthOtpCode.cs
+++ src/Entities/AuthOtpCode.cs
@@ -3,8 +3,12 @@ namespace ReforaTec.Api.Entities;
 public class AuthOtpCode
 {
     public int Id { get; set; }
+    
     public int UserId { get; set; }
     public User? User { get; set; }
+    
     public string VerificationCode { get; set; } = string.Empty;
+    public int FailedAttempts { get; set; } = 0;
+    
     public DateTime ExpiresAt { get; set; }
 }
